e6169a22074e6d4ba2e337aaeb31aa152cbf1337
subin <ansubin2004@gmail.com>
2022-07-12 12:53:28 +0900

    feat: Add Args in Mutation

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Resolver(of => Restaurant)
@@ -7,5 +7,14 @@ export class RestaurantResolver {
   restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
     // console.log(veganOnly);
     return [];
+  }
+  @Mutation(returns => Boolean)
+  createRestaurant(
+    @Args('name') name: string,
+    @Args('isVegan') isVegan: boolean,
+    @Args('address') address: string,
+    @Args('ownersName') ownersName: string,
+  ): boolean {
+    return true;
   }
 }
