33eb04575ab32b24eab2a4eb8408a645e68c8563
subin <ansubin2004@gmail.com>
2022-07-20 08:23:18 +0900

    feat: Add Mutation Args

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,5 +1,6 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { UpdateRestaurantDto } from './dtos/update-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
 import { RestaurantService } from './restaurants.service';
 
@@ -22,5 +23,14 @@ export class RestaurantResolver {
       console.log(e);
       return false;
     }
+  }
+
+  @Mutation(returns => Boolean)
+  async updateRestaurant(
+    // 인자를 많이 넘겨줘야 하기 때문에 효율적이지 않을 수 있다.
+    @Args('id') id: number,
+    @Args('data') data: UpdateRestaurantDto,
+  ) {
+    return true;
   }
 }
