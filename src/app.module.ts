20b546cafa22ea1dee0c433804415647ce635dd8
subin <ansubin2004@gmail.com>
2022-07-22 23:28:15 +0900

    feat: Add password hash

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { User } from './users/entities/user.entity';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.',
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
       ignoreEnvFile: process.env.NODE_ENV === 'prod',
       validationSchema: Joi.object({
         NODE_ENV: Joi.string().valid('dev', 'prod').required(),
