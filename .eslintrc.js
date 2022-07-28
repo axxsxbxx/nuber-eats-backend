7bdd1907ae559d232dbc180341249a86e2d76e49
subin <ansubin2004@gmail.com>
2022-07-28 22:40:24 +0900

    chore: Change version of typeorm

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
+    tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
@@ -26,7 +26,7 @@ module.exports = {
       'error',
       {
         endOfLine: 'auto',
-      }
+      },
     ],
   },
 };
