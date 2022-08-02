28390efcffa8a0728d4587c32f4032d52c7832ac
subin <ansubin2004@gmail.com>
2022-08-02 13:05:34 +0900

    feat: Add verifying user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -51,7 +51,10 @@ export class UsersService {
     // check if the password is correct
     // make a JWT and give it to the user
     try {
-      const user = await this.users.findOne({ where: { email } });
+      const user = await this.users.findOne(
+        { email },
+        { select: ['id', 'password'] },
+      );
       if (!user) {
         return {
           ok: false,
@@ -96,5 +99,24 @@ export class UsersService {
       user.password = password;
     }
     return this.users.save(user);
+  }
+
+  async verifyEmail(code: string): Promise<boolean> {
+    try {
+      const verification = await this.verifications.findOne(
+        { code },
+        { relations: ['user'] },
+        // { loadRelationIds: true },
+      );
+      if (verification) {
+        verification.user.verified = true;
+        this.users.save(verification.user);
+        return true;
+      }
+      throw new Error();
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
   }
 }
